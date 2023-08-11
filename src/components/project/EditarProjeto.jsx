6a13576bfd6f7cabd87b93cf0c206f8b9ff9613d
Dayvid <dayvidacademico@gmail.com>
2023-08-11 12:02:42 -0300

    Creating the input for services and styling the cards.

--- src/components/project/EditarProjeto.jsx
+++ src/components/project/EditarProjeto.jsx
@@ -1,20 +1,30 @@
 /* eslint-disable react/prop-types */
-import { FormControl,FormLabel, Input, FormHelperText} from "@chakra-ui/react";
+ import { FormControl,FormLabel, Input, FormHelperText} from "@chakra-ui/react";
 import { useState } from "react";
+import { supabase } from "../../services/createCliente";
 
-const EditarProjeto = ({ idProject, nomeProject, orcamentoProject, categoriaProject}) =>{
 
-    const{project, setProject} = useState({id:idProject, nome:nomeProject , orcamento:orcamentoProject, id_tipo:categoriaProject })
+const EditarProjeto = ({ idProject, nomeProject, orcamentoProject, servicosProject, categoriaProject}) =>{
+
+    const{project, setProject} = useState({id:idProject, nome:nomeProject , orcamento:orcamentoProject, servicos:servicosProject, id_tipo:categoriaProject })
+
+    
+
+
+
+   
 
     return (
+
   <FormControl>
   <FormLabel> Nome do Projeto</FormLabel>
-  <Input type='text' id={idProject} value={nomeProject} onChange={(e)=>setProject(e.target.value)} />
+  <Input type='text' id={idProject} value={nomeProject}/>
   <FormHelperText> We ll never share your email.</FormHelperText>
 </FormControl>
+
 
   
 
     );
 }
-export default EditarProjeto;
+export default EditarProjeto; 
